724417f825a8b8fdc4fe47b29110336e0e7c06c0
Matt Speaks <mattspeaks@hotmail.com>
2024-01-18 21:30:12 -0600

    Thymeleaf Views Studio

--- src/main/java/org/launchcode/controllers/SpaDayController.java
+++ src/main/java/org/launchcode/controllers/SpaDayController.java
@@ -55,6 +55,12 @@ public class SpaDayController {
         facials.add("Hydrofacial");
         facials.add("Rejuvenating");
         facials.add("Enzyme Peel");
+        model.addAttribute("name", name);
+        model.addAttribute("skintype", skintype);
+        model.addAttribute("manipedi", manipedi);
+
+
+
 
         ArrayList<String> appropriateFacials = new ArrayList<>();
         for (int i = 0; i < facials.size(); i ++) {
@@ -62,7 +68,7 @@ public class SpaDayController {
                 appropriateFacials.add(facials.get(i));
             }
         }
-
+        model.addAttribute("appropriateFacials", appropriateFacials);
         return "menu";
     }
 }
